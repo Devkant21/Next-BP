c3838281896873a7b74a57ce3d22e6272951779a
Devkant <devkant21@gmail.com>
2022-02-28 23:23:34 +0530

    Added other Pages

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,22 +1,16 @@
 import Link from 'next/link'
 // import Navbar from './Navbar'
-import About from '../pages/About'
+// import About from '../pages/About'
 
 const Navbar = () => {
     return (
         <div>
             <nav className='mx-2 my-2'>
                 <ul className='flex px-4 my-4'>
-                    <Link href="/">
-                        <li>Home</li></Link>
-
+                    <Link href="/"><li>Home</li></Link>
                     <Link href="/About"><li>About</li></Link>
-                    <li>
-                        Contact
-                    </li>
-                    <li>
-                        FAQ
-                    </li>
+                    <Link href="/Contact"><li>Contact</li></Link>
+                    <Link href="/FAQ"><li>FAQ</li></Link>
                 </ul>
             </nav>
         </div>
