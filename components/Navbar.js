852b944a66b932c929023faa90d6c31578f17ed7
Devkant <devkant21@gmail.com>
2022-03-01 00:16:36 +0530

    tailwind nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,28 @@
 import Link from 'next/link'
-// import Navbar from './Navbar'
-// import About from '../pages/About'
+import { Transition } from '@headlessui/react'
 
 const Navbar = () => {
     return (
-        <div>
-            <nav className='mx-2 my-2'>
-                <ul className='flex px-4 my-4'>
-                    <Link href="/"><li>Home</li></Link>
-                    <Link href="/About"><li>About</li></Link>
-                    <Link href="/Contact"><li>Contact</li></Link>
-                    <Link href="/FAQ"><li>FAQ</li></Link>
-                </ul>
-            </nav>
-        </div>
+         <nav className=' shadow-sm fixed w-full z-10'>
+            <div className='w-full'>
+                <div className='flex items-center h-20 w-full'>
+                    <div className='flex items-center mx-20 justify-between w-full'>
+                        <div className='flex justify-center items-center flex-shrink-0'>
+                            <ul className='flex'>
+                                <Link href="/">
+                                    <li className='mx-2 cursor-pointer'>Home</li></Link>
+                                <Link href="/About">
+                                    <li className='mx-2 cursor-pointer'>About</li></Link>
+                                <Link href="/Contact">
+                                    <li className='mx-2 cursor-pointer'>Contact</li></Link>
+                                <Link href="/FAQ">
+                                    <li className='mx-2 cursor-pointer'>FAQ</li></Link>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+        </nav>
     )
 }
 
