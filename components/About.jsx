5e3c2e4f4cae7f567ce9385c847b9380c8a54207
Devkant <devkant21@gmail.com>
2022-09-11 23:42:26 +0530

    fixed : apos

--- components/About.jsx
+++ components/About.jsx
@@ -10,31 +10,31 @@ const About = () => {
                 <h2 className='py-4'>Subheading</h2>
                 <p className='py-4 text-gray-800'>
                 Every time your eyes close, I feel my body disappear
-                My fingers start fading then I'm gone, gone, gone
+                My fingers start fading then I &apos;m gone, gone, gone
                 I wake up in the deepest corner of your mind and wonder why
-                I'm living in your world instead of mine
+                I &apos;m living in your world instead of mine
                 Yours instead of mine
-                Even if I don't know quite exactly where I want to be
-                I'm not gonna stay here stuck in someone else's dream
-                Maybe I'll find mine's a nightmare, but I don't even care
+                Even if I don &apos;t know quite exactly where I want to be
+                I &apos;m not gonna stay here stuck in someone else &apos;s dream
+                Maybe I &apos;ll find mine &apos;s a nightmare, but I don &apos;t even care
                 I just wanna live inside my own dream
                 I run for the horizon to see if I can shoot straight off again
                 Would it even wake you if I fall, fall, fall?
                 Throwing rocks at your golden sky
                 But they fall so short I have to laugh
                 Make a little echo in your mind
-                Even if I don't know quite exactly where I want to be
-                I'm not gonna stay here stuck in someone else's dream
-                Maybe I'll find mine's a nightmare, but I don't even care
+                Even if I don &apos;t know quite exactly where I want to be
+                I &apos;m not gonna stay here stuck in someone else &apos;s dream
+                Maybe I &apos;ll find mine &apos;s a nightmare, but I don &apos;t even care
                 I just wanna live inside my own dream
                 I just wanna live inside my own dream
-                Even if I don't know quite exactly where I want to be
-                I'm not gonna stay here stuck in someone else's dream
-                Maybe I'll find mine's a nightmare, but I don't even care
+                Even if I don &apos;t know quite exactly where I want to be
+                I &apos;m not gonna stay here stuck in someone else &apos;s dream
+                Maybe I &apos;ll find mine &apos;s a nightmare, but I don &apos;t even care
                 I just wanna live inside my own
-                Even if I don't know quite exactly where I want to be
-                I'm not gonna stay here stuck in someone else's dream
-                Maybe I'll find mine's a nightmare, but I don't even care
+                Even if I don &apos;t know quite exactly where I want to be
+                I &apos;m not gonna stay here stuck in someone else &apos;s dream
+                Maybe I &apos;ll find mine &apos;s a nightmare, but I don &apos;t even care
                 I just wanna live inside my own
                 </p>
             </div>
